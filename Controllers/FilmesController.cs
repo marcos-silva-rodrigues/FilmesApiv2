3c5100e281b127f39d3a6e228e06ccfe55b6238c
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2023-07-29 19:12:53 -0300

    feat: get and post operations

---
+++ Controllers/FilmesController.cs
@@ -0,0 +1,37 @@
+using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+
+    private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
+
+    [HttpPost]
+    public IActionResult AdicionarFilme([FromBody] Filme filme)
+    {
+        filme.Id = id++;
+        filmes.Add(filme);
+        return CreatedAtAction(nameof(RecuperaFilmePorId),
+                new { id = filme.Id },
+                filme);
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> VerFilme([FromQuery] int skip = 0, [FromQuery] int take = 50)
+    {
+        return filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult RecuperaFilmePorId(int id)
+    {
+        Filme? filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound();
+        return Ok(filme);
+    }
+}
